85cf192cd4120ba2346f0ac8d4cd6ad313595f48
Tomoyuki Nakashima <rr@sgraph.com>
2024-10-28 23:29:50 +0900

    Added `rpi_setup_pwm-fan.sh`

---
+++ rpi_setup_pwm-fan.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+#---------------------------------------
+# Setup `pwm-fan` on hosts.
+#
+# [USAGE]
+#   - `./rpi_setup_pwm-fan.sh [OPTIONS]`
+#
+# [OPTIONS]
+#   - `-t HOSTNAME`
+#     or
+#     `-t USER@HOSTNAME`
+#   - If `-t` option is not specified, `targets` file is used instead.
+#---------------------------------------
+
+set -u
+
+function begin_proc {
+    :
+}
+
+function end_proc {
+    :
+}
+
+function do_proc {
+    if [ $# -eq 1 ]
+    then
+        target=$1
+        host=${target#*@}
+        if [ -f local/pwm-fan/pwm-fan.${host}.dts ] ; then
+            echo "--- ${target} ---"
+            ssh ${target} "mkdir -p local/pwm-fan"
+            scp local/pwm-fan/pwm-fan.${host}.dts ${target}:local/pwm-fan/pwm-fan.dts
+            ssh ${target} "cd local/pwm-fan && cpp -nostdinc -undef -x assembler-with-cpp -I /usr/src/linux-headers-6.6.51+rpt-common-rpi/include/ -I /usr/src/linux-headers-6.6.47+rpt-common-rpi/include/ -o pwm-fan.tmp.dts pwm-fan.dts && dtc -O dtb -o pwm-fan.dtbo pwm-fan.tmp.dts && sudo cp pwm-fan.dtbo /boot/firmware/overlays/ && ls -l /boot/firmware/overlays/pwm-fan.dtbo && sudo reboot"
+        fi
+    fi
+}
+
+source rpi__main.sh

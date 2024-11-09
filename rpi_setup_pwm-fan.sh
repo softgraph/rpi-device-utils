5507da7eddafb82f0db5252068693a4f498ab8a9
Tomoyuki Nakashima <rr@sgraph.com>
2024-11-09 18:53:25 +0900

    Updated `rpi_setup_pwm-fan.sh` to check if `config.txt` needs to be modified

--- rpi_setup_pwm-fan.sh
+++ rpi_setup_pwm-fan.sh
@@ -32,7 +32,20 @@ function do_proc {
             echo "--- ${target} ---"
             ssh ${target} "mkdir -p local/pwm-fan"
             scp local/pwm-fan/pwm-fan.${host}.dts ${target}:local/pwm-fan/pwm-fan.dts
-            ssh ${target} "cd local/pwm-fan && cpp -nostdinc -undef -x assembler-with-cpp -I /usr/src/linux-headers-6.6.51+rpt-common-rpi/include/ -I /usr/src/linux-headers-6.6.47+rpt-common-rpi/include/ -o pwm-fan.tmp.dts pwm-fan.dts && dtc -O dtb -o pwm-fan.dtbo pwm-fan.tmp.dts && sudo cp pwm-fan.dtbo /boot/firmware/overlays/ && ls -l /boot/firmware/overlays/pwm-fan.dtbo && sudo reboot"
+            ssh ${target} \
+                "grep dtoverlay=pwm-fan /boot/firmware/config.txt > /dev/null ; \
+                if [ \$? -eq 1 ] ; then \
+                    echo [ERROR] Add the following line to \'${host}:/boot/firmware/config.txt\'. ; \
+                    echo dtoverlay=pwm-fan ; \
+                else \
+                    cd local/pwm-fan && \
+                    cpp -nostdinc -undef -x assembler-with-cpp -I /usr/src/linux-headers-6.6.51+rpt-common-rpi/include/ -I /usr/src/linux-headers-6.6.47+rpt-common-rpi/include/ -o pwm-fan.tmp.dts pwm-fan.dts && \
+                    dtc -O dtb -o pwm-fan.dtbo pwm-fan.tmp.dts && \
+                    sudo cp pwm-fan.dtbo /boot/firmware/overlays/ && \
+                    ls -l /boot/firmware/overlays/pwm-fan.dtbo && \
+                    sudo reboot ; \
+                fi"
+            #ssh ${target} ""
         fi
     fi
 }

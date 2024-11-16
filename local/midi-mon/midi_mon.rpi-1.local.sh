814ec3086c091b098cdb8eee6dc4e9c55a660eed
Tomoyuki Nakashima <rr@sgraph.com>
2024-11-16 16:17:07 +0900

    Updated `midi_mon.sh`

--- local/midi-mon/midi_mon.rpi-1.local.sh
+++ local/midi-mon/midi_mon.rpi-1.local.sh
@@ -6,8 +6,15 @@
 # [USAGE]
 # - `midi_mon.sh &`
 #
+# [REQUIREMENT]
+# - `aconnect_x.pl`
+#
 # [NOTE]
-# - Edit `S_AUTO_CONNECT_SRC` and `S_AUTO_CONNECT_DST` below to enable automatic connector.
+# - Edit the following lines to enable automatic connector.
+#   - `S_AUTO_CONNECT_SRC`: The name of the source port.
+#   - `S_AUTO_CONNECT_DST`: The name of the destination port.
+#   - `S_AUTO_CONNECT_OPT`: Option for the `aconnect` command.
+# - Run `aconnect` and `aconnect -l` for the details.
 #----------------------------------------
 
 D_TMP=/var/tmp/local/
@@ -16,6 +23,7 @@ F_CON="${D_TMP}midi-con.txt"
 
 S_AUTO_CONNECT_SRC="MPK mini 3 MIDI 1"
 S_AUTO_CONNECT_DST="U2MIDI Pro MIDI 1"
+S_AUTO_CONNECT_OPT=""
 
 mkdir -p $D_TMP
 
@@ -25,8 +33,8 @@ while true ; do
 		P_SRC=`awk '/Out  [0-9]+:[0-9]+ '"$S_AUTO_CONNECT_SRC"'/ { print $2 }' $F_MON`
 		P_DST=`awk '/In   [0-9]+:[0-9]+ '"$S_AUTO_CONNECT_DST"'/ { print $2 }' $F_MON`
 		if [ -n "$P_SRC" ] && [ -n "$P_DST" ] ; then
-			#echo aconnect "$P_SRC" "$P_DST"
-			aconnect "$P_SRC" "$P_DST"
+			#echo aconnect $S_AUTO_CONNECT_OPT $P_SRC $P_DST
+			aconnect $S_AUTO_CONNECT_OPT $P_SRC $P_DST
 			./aconnect_x.pl | tee "$F_MON" | awk '/^Conn / { sub("^Conn ", ""); print }' - > "$F_CON"
 		fi
 	fi

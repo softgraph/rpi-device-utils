04952e987291684d6ed8a5f5d4af054c68478572
Tomoyuki Nakashima <rr@sgraph.com>
2024-11-27 02:44:46 +0900

    Fixed oled-mon's error on system boot.

--- rpi_setup_oled_mon.sh
+++ rpi_setup_oled_mon.sh
@@ -34,29 +34,19 @@ function do_proc {
                 mkdir -p ~/.config/systemd/user ; \
                 mkdir -p ~/local/oled-mon ; \
                 systemctl --user stop oled_mon ; \
+                systemctl --user disable oled_mon ; \
                 pkill -f 'python oled_mon\.py' \
             "
             scp local/oled-mon/demo_opts.py             ${target}:local/oled-mon/
             scp local/oled-mon/oled_mon.${host}.py      ${target}:local/oled-mon/oled_mon.py
             scp local/oled-mon/oled_mon.${host}.service ${target}:local/oled-mon/oled_mon.service
-            if [ -f local/oled-mon/oled_mon_delay.${host}.service ] ; then
-                scp local/oled-mon/oled_mon_delay.${host}.service ${target}:local/oled-mon/oled_mon_delay.service
-            fi
             ssh ${target} " \
                 cd ~/.config/systemd/user && \
                 ln -fs ~/local/oled-mon/oled_mon.service . ; \
-                systemctl --user enable oled_mon && \
-                systemctl --user start  oled_mon ; \
+                systemctl --user enable oled_mon ; \
+                systemctl --user start oled_mon ; \
                 loginctl enable-linger ${user} \
             "
-            if [ -f local/oled-mon/oled_mon_delay.${host}.service ] ; then
-            ssh ${target} " \
-                cd ~/.config/systemd/user && \
-                ln -fs ~/local/oled-mon/oled_mon_delay.service . ; \
-                systemctl --user enable oled_mon_delay && \
-                systemctl --user start  oled_mon_delay \
-            "
-            fi
             ssh ${target} " \
                 ps -x -o pid,ppid,user,cmd | \
                 grep -v grep | \

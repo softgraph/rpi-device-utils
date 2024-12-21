17c2f580317e534f0b6de2d1f1398297a22befcf
Tomoyuki Nakashima <rr@sgraph.com>
2024-12-21 13:29:51 +0900

    Updated `disp_contents.py` to support text alignments

--- local/disp-mon/disp_contents.py
+++ local/disp-mon/disp_contents.py
@@ -12,13 +12,34 @@ from glob import iglob
 import time
 
 from luma.core.render import canvas # type: ignore
+from PIL import ImageFont # type: ignore
+
+DURATION_PER_PAGE = 2
+
+TEXT_ALIGN_LEFT = 0
+TEXT_ALIGN_CENTER = 1
+TEXT_ALIGN_RIGHT = 2
+
+TEXT_VALIGN_TOP = 0
+TEXT_VALIGN_CENTER = 1
+TEXT_VALIGN_BOTTOM = 2
 
 disp_lines = deque([])
 disp_temperatures = deque([], maxlen = 128)
 
-DURATION_PER_PAGE = 2
+bounding_box = None
+default_font = None
+
+def load_defaults(device):
+    global bounding_box
+    global default_font
+    if bounding_box == None:
+        bounding_box = device.bounding_box
+    if default_font == None:
+        default_font = ImageFont.load_default()
 
 def monitor(device, contextName, width, height):
+    load_defaults(device)
     while True:
         update_disp_lines()
         page_num = 1
@@ -63,29 +84,29 @@ def update_disp_temperatures():
 
 def draw_page(dc, width, height, page):
     if height == 32:
-        draw_header(dc, base_y = 0)
+        draw_header(dc, width, base_y = 0)
         if page == 0:
-            draw_temperature_map(dc, base_y = 0)
+            draw_temperature_map(dc, width, base_y = 0)
         else:
-            draw_line(dc, base_y = 16, line = page - 1)
+            draw_line(dc, width, base_y = 16, index = page - 1)
     elif height == 64:
-        draw_header(dc, base_y = 0)
-        draw_temperature_map(dc, base_y = 0)
-        draw_line(dc, base_y = 32, line = page * 2)
-        draw_line(dc, base_y = 48, line = page * 2 + 1)
+        draw_header(dc, width, base_y = 0)
+        draw_temperature_map(dc, width, base_y = 0)
+        draw_line(dc, width, base_y = 32, index = page * 2)
+        draw_line(dc, width, base_y = 48, index = page * 2 + 1)
     else:
-        draw_header(dc, base_y = 0)
+        draw_header(dc, width, base_y = 0)
 
-def draw_header(dc, base_y):
+def draw_header(dc, width, base_y):
     now = datetime.datetime.now()
     str_time = now.strftime("%H:%M:%S")
     temp = disp_temperatures[-1]
     str_temp = "{:.2f} °C".format(temp / 1000)
-    # dc.rectangle(device.bounding_box, outline="white")
-    dc.text((85, base_y - 1), str_time, fill="white")
-    dc.text(( 0, base_y - 1), str_temp, fill="white")
+    # dc.rectangle(bounding_box, outline = "white")
+    draw_text(dc, width, base_y, str_temp, TEXT_ALIGN_LEFT, TEXT_VALIGN_TOP)
+    draw_text(dc, width, base_y, str_time, TEXT_ALIGN_RIGHT, TEXT_VALIGN_TOP)
 
-def draw_temperature_map(dc, base_y):
+def draw_temperature_map(dc, width, base_y):
     x = 0
     for temp in disp_temperatures:
         y = - int(temp / 1000) + 66
@@ -96,8 +117,36 @@ def draw_temperature_map(dc, base_y):
         dc.point((x, base_y + y), fill="white")
         x += 1
 
-def draw_line(dc, base_y, line):
-    if line >= 0 and line < len(disp_lines):
-        dc.text((0, base_y + 5), disp_lines[line], fill="white")
+def draw_line(dc, width, base_y, index):
+    if index >= 0 and index < len(disp_lines):
+        line = disp_lines[index]
+        if len(line) > 1 and line[0] == "\t":
+            if len(line) > 2 and line[1] == "\t":
+                line = line.lstrip()
+                draw_text(dc, width, base_y, line, TEXT_ALIGN_RIGHT, TEXT_VALIGN_BOTTOM)
+            else:
+                line = line.lstrip()
+                draw_text(dc, width, base_y, line, TEXT_ALIGN_CENTER, TEXT_VALIGN_BOTTOM)
+        else:
+            draw_text(dc, width, base_y, line, TEXT_ALIGN_LEFT, TEXT_VALIGN_BOTTOM)
+
+def draw_text(dc, width, base_y, text, align, valigh):
+    if align == TEXT_ALIGN_RIGHT:
+        font_width = default_font.getlength(text)
+        font_width += len(text) * 0.25 # magic
+        x = (width - font_width) // 1
+    elif align == TEXT_ALIGN_CENTER:
+        font_width = default_font.getlength(text)
+        font_width += len(text) * 0.25 # magic
+        x = (width - font_width) // 2
+    else:
+        x = 0
+    if valigh == TEXT_VALIGN_BOTTOM:
+        y = base_y + 5
+    elif valigh == TEXT_VALIGN_CENTER:
+        y = base_y + 2
+    else:
+        y = base_y - 1
+    dc.text((x, y), text, fill = "white", font = default_font)
 
 #----------------------------------------

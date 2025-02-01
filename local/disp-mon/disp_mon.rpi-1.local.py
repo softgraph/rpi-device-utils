121e34a8ea9e8bf30afa2b6fbff3cf1afe5dc151
Tomoyuki Nakashima <rr@sgraph.com>
2025-02-01 13:22:21 +0900

    Added the GPIO keys to activate and deactivate midi-mon

--- local/disp-mon/disp_mon.rpi-1.local.py
+++ local/disp-mon/disp_mon.rpi-1.local.py
@@ -19,9 +19,11 @@ import midi_mon # type: ignore
 #----------------------------------------
 
 _EVENT_QUIT = event_mon.EVENT_QUIT
-_EVENT_INCREMENT_AND_DRAW = event_mon.EVENT_COUNT_1000_MS
-_EVENT_UPDATE_AND_DRAW = event_mon.EVENT_COUNT_300_MS
+_EVENT_INCREMENT = event_mon.EVENT_COUNT_1000_MS
+_EVENT_MIDI_UPDATE = event_mon.EVENT_COUNT_300_MS
+_EVENT_MIDI_ACTIVATE = event_mon.EVENT_GPO_KEY_DEPRESSED_0
 _EVENT_MIDI_TOGGLE = event_mon.EVENT_GPO_KEY_DEPRESSED_0 + 1
+_EVENT_MIDI_DEACTIVATE = event_mon.EVENT_GPO_KEY_DEPRESSED_0 + 2
 
 #----------------------------------------
 # Non-public Variables
@@ -47,10 +49,16 @@ def _main():
             event = event_mon.get_event()
             if event == _EVENT_QUIT:
                 break
-            elif event == _EVENT_INCREMENT_AND_DRAW:
+            elif event == _EVENT_INCREMENT:
                 i = disp_contents.increment_and_draw(i)
-            elif event == _EVENT_UPDATE_AND_DRAW:
+            elif event == _EVENT_MIDI_UPDATE:
                 if midi_mon.update():
+                    i = disp_contents.update_and_draw()
+            elif event == _EVENT_MIDI_ACTIVATE:
+                if midi_mon.activate():
+                    i = disp_contents.update_and_draw()
+            elif event == _EVENT_MIDI_DEACTIVATE:
+                if midi_mon.deactivate():
                     i = disp_contents.update_and_draw()
             elif event == _EVENT_MIDI_TOGGLE:
                 if midi_mon.toggle():
